2b6d31d8e3eb2a601fe9de21d80d038171fb7bed
Alicia Hurst <alicia@aliciahurst.com>
2015-04-28 12:18:45 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Fayetteville Coworking is a new community for independent workers in Fayetteville, NC area, including Hope Mills, Lumberton, Sanford, and Fort Bragg."/>
+	<title>Fayetteville Coworking | Fayetteville, NC</title>
+	<link rel="canonical" href="http://fayettevillecoworking.com" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Fayetteville Coworking" />
+	<meta property="og:description" content="Fayetteville Coworking is a new community for independent workers in Fayetteville, NC area, including Hope Mills, Lumberton, Sanford, and Fort Bragg." />
+	<meta property="og:url" content="http://fayettevillecoworking.com" />
+	<meta property="og:site_name" content="Fayetteville Coworking" />
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel='stylesheet' href='/css/main.css' type='text/css' media='all' />
+	<link href='http://fonts.googleapis.com/css?family=Nunito:300,400' rel='stylesheet' type='text/css'>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45058744-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body>
+
+	<header>
+	<h1>Fayetteville Coworking</h1>
+
+	<h2>Work Independently, Not Alone</h2>
+
+	<p>We combat the loneliness and distractions of working from home or alone at a coffee shop by working together.</p>
+
+	<p>Every week independent workers gather during the workday to work alongside each other for real human interaction, productivity, and community.</p>
+
+	<p><strong>Will you be there?</strong></p>
+	</header>
+
+	<div class="photos">
+	<img src="img/1.jpg" alt="Fayetteville Coworking">
+	</div>
+
+	<nav>
+		<ul>
+			<li><a href="#" target="_blank">Subscribe to our mailing list</a></li>
+			<li><a href="http://facebook.com/groups/fayettevillecoworking" target="_blank">Join our Facebook group</a></li>
+			<li><a href="http://twitter.com/faycoworking" target="_blank">Follow us on Twitter</a></li>
+			<li><a href="#" target="_blank">See our upcoming events on Google Calendar</a></li>
+			<li><a href="mailto:info@fayettevillecoworking.com" target="_blank">Contact us via email</a></li>
+		</ul>
+	</nav>
+
+</body>
+</html>
